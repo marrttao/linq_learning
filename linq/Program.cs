7660f4436403a7f30a6787c26f1973a7a6f5ed85
marrt <marrttaoburberry@gmail.com>
2025-05-06 23:09:55 +0300

    completed task 4

--- linq/Program.cs
+++ linq/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string [] strings = { "one", "two", "three" };
+        IEnumerable<string> UserSortDescending =
+            from s in strings
+            orderby s.Length descending
+            select s;
+        IEnumerable<string> UserSortAscending =
+            from s in strings
+            orderby s.Length
+            select s;
     }
+    
 }
--- tests/Test1.cs
+++ tests/Test1.cs
@@ -1,10 +1,30 @@
-﻿namespace tests;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
 
-[TestClass]
-public sealed class Test1
+namespace tests
 {
-    [TestMethod]
-    public void TestMethod1()
+    [TestClass]
+    public sealed class LinqTests
     {
+        private readonly string[] strings = { "twooo", "one", "threee" };
+
+        [TestMethod]
+        public void Test_UserSortDescending()
+        {
+            IEnumerable<string> expected = new[] { "threee", "twooo", "one" }; // Ordered by length descending
+            IEnumerable<string> actual = from s in strings orderby s.Length descending select s;
+
+            CollectionAssert.AreEqual(expected.ToList(), actual.ToList());
+        }
+
+        [TestMethod]
+        public void Test_UserSortAscending()
+        {
+            IEnumerable<string> expected = new[] { "one", "twooo", "threee" }; // Ordered by length ascending
+            IEnumerable<string> actual = from s in strings orderby s.Length select s;
+
+            CollectionAssert.AreEqual(expected.ToList(), actual.ToList());
+        }
     }
 }
