b4fa248003830b461dad8589c5134bb35ac13a9d
marrt <marrttaoburberry@gmail.com>
2025-05-10 16:12:05 +0300

    completed task 4

--- linq/Program.cs
+++ linq/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string [] numbers = { "11", "35", "78", "12", "98" };
+        var sortBySumOfNumber = 
+            from number in numbers
+            where number.Length == 2
+            let sum = number[0] - '0' + number[1] - '0'
+            orderby sum
+            select sum;
+        var AnotherSortBySumOfNumber = 
+            from number in numbers
+            where number.Length == 2
+            let sum = number[0] - '0' + number[1] - '0'
+            orderby sum descending 
+            select sum;
+
     }
 }
--- tests/Test1.cs
+++ tests/Test1.cs
@@ -1,10 +1,42 @@
-﻿namespace tests;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace tests;
 
 [TestClass]
-public sealed class Test1
+public class LinqTests
 {
     [TestMethod]
-    public void TestMethod1()
+    public void TestSortBySumOfNumberAscending()
     {
+        // Arrange
+        string[] numbers = { "11", "35", "78", "12", "98" };
+
+        // Act
+        var result = (from number in numbers
+            where number.Length == 2
+            let sum = number[0] - '0' + number[1] - '0'
+            orderby sum
+            select sum).ToArray();
+
+        // Assert
+        CollectionAssert.AreEqual(new[] { 2, 3, 8, 15, 17 }, result);
+    }
+
+    [TestMethod]
+    public void TestSortBySumOfNumberDescending()
+    {
+        // Arrange
+        string[] numbers = { "11", "35", "78", "12", "98" };
+
+        // Act
+        var result = (from number in numbers
+            where number.Length == 2
+            let sum = number[0] - '0' + number[1] - '0'
+            orderby sum descending
+            select sum).ToArray();
+
+        // Assert
+        CollectionAssert.AreEqual(new[] { 17, 15, 8, 3, 2 }, result);
     }
 }
